4d8ea7201805441cf4238d3c9b69e0010b699f20
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-01 19:25:04 +0200

    refactor(logger): don't define the dataset in the header if the convar for it isn't set -> will let fivemanage determine the "default" dataset to use

--- imports/logger/server.lua
+++ imports/logger/server.lua
@@ -94,7 +94,7 @@ end
 
 if service == 'fivemanage' then
     local key = GetConvar('fivemanage:key', '')
-    local dataset = GetConvar('fivemanage:dataset', 'default')
+    local dataset = GetConvar('fivemanage:dataset', '')
 
     if key ~= '' then
         local endpoint = 'https://api.fivemanage.com/api/logs/batch'
@@ -103,8 +103,11 @@ if service == 'fivemanage' then
             ['Content-Type'] = 'application/json',
             ['Authorization'] = key,
             ['User-Agent'] = 'ox_lib',
-            ['X-Fivemanage-Dataset'] = dataset,
         }
+
+        if dataset ~= "" then
+            headers['X-Fivemanage-Dataset'] = dataset
+        end
 
         function lib.logger(source, event, message, ...)
             if not buffer then
