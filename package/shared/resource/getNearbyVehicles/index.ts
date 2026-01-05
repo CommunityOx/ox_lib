b4018e760379d8ccc404b854b22b63932a158b92
Dilax <88612962+MrDilaX@users.noreply.github.com>
2026-01-05 12:14:32 +0530

    sync with cox fork (05/01/2026)

--- package/shared/resource/getNearbyVehicles/index.ts
+++ package/shared/resource/getNearbyVehicles/index.ts
@@ -1,10 +1,7 @@
 import { cache } from '../cache/index';
 import { context } from '../../index';
-<<<<<<< HEAD
-import { Vector3 } from "@nativewrappers/client";
-=======
+//import { Vector3 } from "@nativewrappers/client";
 import { Vector3 } from "@nativewrappers/fivem";
->>>>>>> upstream/main
 
 interface NearbyVehicle {
     vehicle: number;
@@ -30,8 +27,4 @@ export function getNearbyVehicles(coords: Vector3, maxDistance: number = 2.0, in
     }
 
     return nearbyVehicles;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> upstream/main
